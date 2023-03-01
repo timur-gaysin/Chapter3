ee64783064dee362f798351af04eb13eee4d8cd3
Timur Gaysin <93344539+timur-gaysin@users.noreply.github.com>
2023-03-01 18:03:27 +0300

    add return chapter
    passing parameters and returning a value from a trailing closure

--- TrailingClosurePlayground.playground/Contents.swift
+++ TrailingClosurePlayground.playground/Contents.swift
@@ -1,5 +1,32 @@
-func simppleExample(closure:() -> Void){
+func simpleExample(closure:() -> Void){
     print("1. Wake up")
     closure()
     print("4. Eat breakfast")
+}
+
+simpleExample(){
+    print("---2. Go to bathroom")
+    print("---3. Brush teeth")
+}
+
+func passParameters(closure: (Int, Int) -> Void){
+    print("First line")
+    closure(2,8)
+    print("First line")
+}
+
+passParameters{ x,y in
+    print("-- closure code begining")
+    print("\(x*y)")
+    print("-- ending")
+}
+
+func returnValue(closure: (Int, Int) -> Int){
+    print("First Line")
+    print("\(closure(6,1))")
+    print("Second line")
+}
+
+returnValue{ x,y in
+    x * y
 }
